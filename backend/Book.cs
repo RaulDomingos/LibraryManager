180c8ed2484865825b79f7867956aa23fcbb3398
RaulDomingos <rauldomingos0907@gmail.com>
2024-02-05 22:25:24 -0300

    first commit

---
+++ backend/Book.cs
@@ -0,0 +1,8 @@
+namespace LibraryAPI;
+
+public class Book {
+    public int id_book { get; set; }
+    public string title { get; set; }
+    public string description { get; set; }
+    public string url { get; set; }
+}
