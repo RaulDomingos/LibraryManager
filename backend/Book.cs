a876eb9a66bb6ee0d8314d63576d4c1caf417164
RaulDomingos <rauldomingos0907@gmail.com>
2024-02-06 00:48:12 -0300

    Database context created

--- backend/Book.cs
+++
@@ -1,8 +0,0 @@
-namespace LibraryAPI;
-
-public class Book {
-    public int id_book { get; set; }
-    public string title { get; set; }
-    public string description { get; set; }
-    public string url { get; set; }
-}
---
+++ backend/backend/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Models {
+    public class Book {
+        public int BookId {  get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+    }
+}
