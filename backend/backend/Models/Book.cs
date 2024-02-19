e6e5d8634caa51dd14e867e027b43a0540d1bb51
RaulDomingos <rauldomingos0907@gmail.com>
2024-02-19 06:37:21 -0300

    backend stack changed. Node.js project started

--- backend/backend/Models/Book.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace backend.Models {
-    public class Book {
-        public int BookId {  get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Picture { get; set; }
-    }
-}
